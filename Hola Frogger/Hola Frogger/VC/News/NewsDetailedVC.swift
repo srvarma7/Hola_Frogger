f89c64d6d1f8977ff0eb37ba9293590b9187d6bd
Sai Raghu Varma <srvarma7@gmail.com>
2020-10-15 14:39:37 +1100

    Adding programmatic News list and News detailed VCs

---
+++ Hola Frogger/Hola Frogger/VC/News/NewsDetailedVC.swift
@@ -0,0 +1,95 @@
+//
+//  NewsDetailedVC.swift
+//  Hola Frogger
+//
+//  Created by Sai Raghu Varma Kallepalli on 14/10/20.
+//  Copyright © 2020 Varma. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class NewsDetailedVC: UIViewController {
+
+    var articleLink: String?
+    
+    private let webView = WKWebView()
+    private let activityIndicator = UIActivityIndicatorView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.navigationDelegate = self
+        
+        configureViews()
+        loadWebPage()
+    }
+    
+    private func configureViews() {
+        view.addSubview(webView)
+        view.addSubview(activityIndicator)
+        
+        addViewConstrians()
+        
+        activityIndicator.startAnimating()
+        activityIndicator.color = UIColor.raspberryTint()
+        activityIndicator.style = .large
+    }
+    
+    private func addViewConstrians() {
+        webView.addAnchor(top: view.topAnchor, paddingTop: 0,
+                          left: view.leftAnchor, paddingLeft: 0,
+                          bottom: view.bottomAnchor, paddingBottom: 0,
+                          right: view.rightAnchor, paddingRight: 0,
+                          width: 0, height: 0, enableInsets: true)
+    
+        webView.backgroundColor = .raspberryTint()
+        
+        activityIndicator.addAnchor(top: webView.topAnchor, paddingTop: 400,
+                                    left: webView.leftAnchor, paddingLeft: 100,
+                                    bottom: webView.bottomAnchor, paddingBottom: 400,
+                                    right: webView.rightAnchor, paddingRight: 100,
+                                    width: 0, height: 0, enableInsets: true)
+    
+    }
+    
+    private func loadWebPage() {
+                
+        guard let url = URL(string: articleLink ?? "https://www.google.com/?client=safari") else { return }
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+        // Starts animation when website is loading.
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+    }
+
+}
+
+extension NewsDetailedVC: WKNavigationDelegate {
+    
+    // When it finishes loading, the animation is stopepd.
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityIndicator.stopAnimating()
+    }
+    
+    // When it fails loading, the animation is stopepd.
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activityIndicator.stopAnimating()
+    }
+    
+    // When it starts loading again, the animation is started.
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        activityIndicator.startAnimating()
+    }
+    
+    #warning("Remove this test method")
+//    private func testWeb() {
+//        let url = URL(string: "https://www.google.com/?client=safari")
+//        let request = URLRequest(url: url!)
+//        webView.load(request)
+//        webView.navigationDelegate = self
+//        // Starts animation when website is loading.
+//        activityIndicator.startAnimating()
+//        activityIndicator.hidesWhenStopped = true
+//    }
+}
---
+++ Hola Frogger/Hola Frogger/VC/News/NewsVC.swift
@@ -0,0 +1,114 @@
+//
+//  NewsVC.swift
+//  Hola Frogger
+//
+//  Created by Sai Raghu Varma Kallepalli on 12/10/20.
+//  Copyright © 2020 Varma. All rights reserved.
+//
+
+import UIKit
+
+class NewsVC: UIViewController {
+    
+    private struct Cell {
+        static let cellId = "cellId"
+    }
+    
+    private var newsViewModel = NewsViewModel()
+    
+    private var tableView           = UITableView()
+    private var activityIndicator   = UIActivityIndicatorView()
+    private var refreshControl      = UIRefreshControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newsViewModel.fetchNewsDelegate = self
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        #warning("Enable fetch method after final")
+//        newsViewModel.fetchNews()
+        
+        setupViews()
+    }
+    
+    private func setupViews() {
+        setupTableView()
+        setupActivityIndicator()
+        setupRefreshControl()
+
+        addConstriants()
+    }
+    
+    private func setupActivityIndicator() {
+        view.addSubview(activityIndicator)
+        activityIndicator.color = UIColor.raspberryTint()
+        activityIndicator.style = .large
+        activityIndicator.startAnimating()
+    }
+    
+    private func setupRefreshControl() {
+        refreshControl.attributedTitle  = NSAttributedString(string: "Pull to refresh")
+        refreshControl.tintColor        = UIColor.raspberryTint()
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        tableView.addSubview(refreshControl)
+    }
+    
+    @objc func refresh(_ sender: Any) {
+        newsViewModel.fetchNews()
+    }
+
+    private func addConstriants() {
+        activityIndicator.addAnchor(top: tableView.topAnchor, paddingTop: 200,
+                                    left: tableView.leftAnchor, paddingLeft: 200,
+                                    bottom: tableView.bottomAnchor, paddingBottom: 200,
+                                    right: tableView.rightAnchor, paddingRight: 200,
+                                    width: 0, height: 0, enableInsets: true)
+        
+    }
+}
+
+// MARK:- News fetching delegate
+extension NewsVC: NewsProtocol {
+    func didFinishFetchingNews() {
+        tableView.reloadData()
+        activityIndicator.stopAnimating()
+        refreshControl.endRefreshing()
+    }
+}
+
+// MARK:- Table view
+extension NewsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        
+        tableView.pin(to: view)
+        setupDelegates()
+        tableView.register(NewsTVCell.self, forCellReuseIdentifier: Cell.cellId)
+    }
+    
+    private func setupDelegates() {
+        tableView.delegate   = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newsViewModel.news.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell  = tableView.dequeueReusableCell(withIdentifier: Cell.cellId) as? NewsTVCell else { return UITableViewCell() }
+        cell.article    = newsViewModel.news[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedArticle         = newsViewModel.news[indexPath.row]
+        let detailedNewsVC          = NewsDetailedVC()
+        detailedNewsVC.articleLink  = selectedArticle.url
+        detailedNewsVC.title        = selectedArticle.title
+        navigationController?.pushViewController(detailedNewsVC, animated: true)
+    }
+}
