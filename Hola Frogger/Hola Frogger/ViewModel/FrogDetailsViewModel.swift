9273ee1dafb9492721c58b885a26232bb0a5e4ff
Sai Raghu Varma <srvarma7@gmail.com>
2020-10-19 16:02:56 +1100

    Adding FrogDetailsViewModel

--- Hola Frogger/Hola Frogger/ViewModel/FrogDetailsViewModel.swift
+++ Hola Frogger/Hola Frogger/ViewModel/FrogDetailsViewModel.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+class FrogDetailsViewModel {
+   
+    var weather: WeatherJsonResponse?
+    var imageData: Data?
+    
+    var weatherDelegate: WeatherProtocol?
+    
+    func fetchWeatherDetailsFor(latitude: String, longitude: String) {
+        
+        let resource = WeatherJsonResponse.urlFor(latitude: latitude, longitude: longitude)
+        WebService.getDataFromAPI(resource: resource, completion: { [weak self]  result in
+            switch result {
+                case .failure(let error):
+                    print(error)
+                    
+            case .success(let data):
+                self?.weather = data
+                self?.getWeatherIcon(iconName: (data?.weather[0].icon)!)
+                self?.weatherDelegate?.didFinishFetchingWeather()
+            }
+        })
+    }
+    
+    private func getWeatherIcon(iconName: String) {
+        WebService.getImage(icon: iconName, completion: { result in
+            switch result {
+            case .success(let data):
+                self.imageData = data
+                self.weatherDelegate?.didFinishFetchingWeatherIcon()
+            case .failure(let error):
+                print("Failure", error)
+                
+            }
+        })
+    }
+    
+}
