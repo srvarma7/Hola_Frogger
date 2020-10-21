e363fa1a7803b5e688681c88cf6b5af3c5610e1b
Sai Raghu Varma <srvarma7@gmail.com>
2020-10-21 16:05:14 +1100

    Adding waves and lottie color to UIcolour extension

--- Hola Frogger/Hola Frogger/Extension/UIColor+Extension.swift
+++ Hola Frogger/Hola Frogger/Extension/UIColor+Extension.swift
@@ -12,4 +12,10 @@ extension UIColor {
     static func raspberryPieTint() -> UIColor {
         return #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
     }
+    static func wavesTint() -> UIColor {
+        return #colorLiteral(red: 0.4943358898, green: 0.8269905448, blue: 0.1309139729, alpha: 1)
+    }
+    static func lottieTint() -> UIColor {
+        return #colorLiteral(red: 0.9791167378, green: 0.8544659019, blue: 0.7218489051, alpha: 1)
+    }
 }
