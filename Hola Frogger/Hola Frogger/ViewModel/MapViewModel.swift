91847ebfad11642dcf11311baa2a889168683c39
Sai Raghu Varma <srvarma7@gmail.com>
2020-10-22 16:26:12 +1100

    Adding MapViewModel to hold required data

---
+++ Hola Frogger/Hola Frogger/ViewModel/MapViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  MapViewModel.swift
+//  Hola Frogger
+//
+//  Created by Sai Raghu Varma Kallepalli on 22/10/20.
+//  Copyright © 2020 Varma. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapViewModel {
+    
+    var allFrogsList = [FrogEntity]()
+    
+    var focusLocation = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -37.937282, longitude: 144.610342), latitudinalMeters: 800000, longitudinalMeters: 800000)
+    var selectedAnnotationTitle = ""
+    
+    init() {
+        refresh()
+    }
+    
+    func refresh() {
+        allFrogsList = CoreDataHandler.fetchAllFrogs()
+        
+        if(allFrogsList.count == 0) {
+            CoreDataHandler.addAllFrogRecordsToDatabase()
+            allFrogsList = CoreDataHandler.fetchAllFrogs()
+        }
+    }
+    
+    func getFrogByName() -> FrogEntity {
+        var foundFrog: FrogEntity!
+        for frog in allFrogsList {
+            if frog.cname == selectedAnnotationTitle {
+                foundFrog = frog
+            }
+        }
+        return foundFrog
+    }
+    
+}
