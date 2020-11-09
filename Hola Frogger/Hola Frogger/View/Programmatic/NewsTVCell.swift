1ae00b254c0c064cf9b6ba3d9d17c05d8cebcb0d
Sai Raghu Varma <srvarma7@gmail.com>
2020-11-09 11:47:57 +1100

    Adding icon next to news heading in NewsVC

--- Hola Frogger/Hola Frogger/View/Programmatic/NewsTVCell.swift
+++ Hola Frogger/Hola Frogger/View/Programmatic/NewsTVCell.swift
@@ -12,6 +12,7 @@ class NewsTVCell: UITableViewCell {
     
     private var heading     = UILabel()
     private var subheading  = UILabel()
+    private var imgView     = UIImageView()
 
     var article: Article? {
         didSet {
@@ -24,6 +25,9 @@ class NewsTVCell: UITableViewCell {
             subheading.font = .systemFont(ofSize: 17)
             subheading.numberOfLines = 0
             subheading.alpha = 0.7
+            
+            imgView.image = UIImage(systemName: "square.and.pencil")
+            imgView.tintColor = .raspberryPieTint()
         }
         
     }
@@ -38,19 +42,26 @@ class NewsTVCell: UITableViewCell {
         
         contentView.addSubview(heading)        
         contentView.addSubview(subheading)
+        contentView.addSubview(imgView)
         
         addConstraints()
     }
     
     private func addConstraints() {
-        heading.addAnchor(top: contentView.topAnchor, paddingTop: 10,
+        imgView.addAnchor(top: contentView.topAnchor, paddingTop: 15,
                           left: contentView.leftAnchor, paddingLeft: 10,
+                          bottom: nil, paddingBottom: 0,
+                          right: nil, paddingRight: 0,
+                          width: 20, height: 20, enableInsets: true)
+        
+        heading.addAnchor(top: contentView.topAnchor, paddingTop: 10,
+                          left: imgView.rightAnchor, paddingLeft: 10,
                           bottom: nil, paddingBottom: 0,
                           right: contentView.rightAnchor, paddingRight: 10,
                           width: 0, height: 0, enableInsets: true)
         
         subheading.addAnchor(top: heading.bottomAnchor, paddingTop: 8,
-                             left: contentView.leftAnchor, paddingLeft: 10,
+                             left: imgView.rightAnchor, paddingLeft: 10,
                              bottom: contentView.bottomAnchor, paddingBottom: 8,
                              right: contentView.rightAnchor, paddingRight: 10,
                              width: 0, height: 0, enableInsets: true)
