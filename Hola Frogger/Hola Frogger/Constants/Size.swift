c1e25360eeabc3a7448a374c761b2ab338e92e85
Sai Raghu Varma <srvarma7@gmail.com>
2020-11-10 15:06:30 +1100

    Adding Size struct to get screen dimensions

---
+++ Hola Frogger/Hola Frogger/Constants/Size.swift
@@ -0,0 +1,18 @@
+//
+//  Size.swift
+//  Hola Frogger
+//
+//  Created by Sai Raghu Varma Kallepalli on 9/11/20.
+//  Copyright © 2020 Varma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Size {
+    static let bounds       = UIScreen.main.bounds
+    static let height       = bounds.height
+    static let width        = bounds.width
+    static let centerWidth  = bounds.width/2
+    static let centerHeight = bounds.height/2
+}
